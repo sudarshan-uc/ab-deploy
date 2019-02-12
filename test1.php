384592746052a382fb42679e2a63e37e4e92f2d0
Sudarshan Chaudhari <30308970+sudarshan-uc@users.noreply.github.com>
2019-02-12 14:13:42 +0530

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,7 +1,7 @@
 <?php
 shell_exec('date >>  /test/1/test-`cat /etc/hostname`.txt')
 for ($x = 0; $x <= 5; $x++) {
-    echo "The number is: $x <br>";\
-    sleep(1)
+    echo "The number is: $x <br>";
+    sleep(1);
 }  
 ?>
