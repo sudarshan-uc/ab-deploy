10a132e44cb0762551f4d88f0b80cae4b426741c
Sudarshan Chaudhari <suchaudh@redhat.com>
2019-11-25 16:29:26 +0530

    Exam Code

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
  <?php 
  echo "I exam pod waiting for the Deployment . . . .";
- echo "Yuu Huuu, I am Running. "
+ echo "Yuu Huuu, I am Running. ";
  echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
  ?>
